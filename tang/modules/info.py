aeceb42e1f7988e9629891d0dfab9f8092b53113
marika <marika@waifu.club>
2022-08-03 05:51:58 -0300

    Fully implement the info module.

--- tang/modules/info.py
+++ tang/modules/info.py
@@ -7,34 +7,57 @@ class InfoModule:
     def __init__(self):
         self.requires_encryption = False
 
-    def avs(__self__):
+    def avs(self):
         """
         Get information about the Arcade Virtual System (AVS) module.
         """
 
         return [
             {
-                'model': 'model',
-                'ext': '-1',
-                'dest': 'dest',
-                'spec': 'spec',
-                'rev': 'rev',
-                'services': 'services',
-                'version': 'version',
+                'model': 'TNG',
+                'ext': '202208031200',
+                'dest': 'A',
+                'spec': 'A',
+                'rev': 'A',
+                'services': 'http://tang.services/',
             },
         ]
 
-    def launcher(__self__):
+    def launcher(self):
         """
         Get information about Spice.
         """
 
         return [
             {
-                'version': 'version',
-                'compile_date': 'compile_date',
-                'compile_time': 'compile_time',
-                'system_time': 'system_time',
-                'args': ['args', 'args', 'args'],
+                'version': '1.0',
+                'compile_date': 'Aug 3 2022',
+                'compile_time': '12:00:00',
+                'system_time': '2022-08-03T12:00:00Z',
+                'args': ['--arg1', '--arg2', '--arg3'],
+            },
+        ]
+
+    def memory(self):
+        """
+        Get information about the system memory.
+        """
+
+#        info.AddMember("mem_total", memutils::mem_total(), alloc);
+#        info.AddMember("mem_total_used", memutils::mem_total_used(), alloc);
+#        info.AddMember("mem_used", memutils::mem_used(), alloc);
+#        info.AddMember("vmem_total", memutils::vmem_total(), alloc);
+#        info.AddMember("vmem_total_used", memutils::vmem_total_used(), alloc);
+#        info.AddMember("vmem_used", memutils::vmem_used(), alloc);
+
+        return [
+            {
+                "mem_total": 8 * 1000 * 1000,
+                "mem_total_used": 4 * 1000 * 1000,
+                "mem_used": 4 * 1000 * 1000,
+
+                "vmem_total": 4 * 1000 * 1000,
+                "vmem_total_used": 1 * 1000 * 1000,
+                "vmem_used": 1 * 1000 * 1000,
             },
         ]
